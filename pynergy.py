d20bda7f1b8f618599464edb965346f55c2326d4
Sean Anderson <sean.martin.anderson@gmail.com>
2014-12-15 14:28:09 -0600

    first attempt, missing search

---
+++ pynergy.py
@@ -0,0 +1,27 @@
+#!/Users/sma/anaconda/bin/python
+"""
+A script for calculating transition energies between bands.
+"""
+
+import sys
+import numpy as np
+
+INPUT = sys.argv[1]
+OUTPUT = sys.argv[2]
+
+EIGEN = np.loadtxt(INPUT)
+KPTS = len(EIGEN)
+BANDS = len(EIGEN[0])
+
+FILE = open(OUTPUT, 'w')
+
+for kpt in range(0, KPTS):
+    for origin in range(1, BANDS):
+        for target in range(origin + 1, BANDS):
+            diff = abs(EIGEN[kpt, origin] - EIGEN[kpt, target])
+            result = 'k-point: {0:0>3d} |' \
+                     ' bands: {2:0>2d} -> {3:0>2d} |' \
+                     ' diff: {1:0>9.6f}\n'\
+                     .format(kpt + 1, diff, origin, target)
+            FILE.write(result)
+FILE.close()
