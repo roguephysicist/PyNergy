832d7622c48b521592da480e889e585f7a2a6f7b
Sean Anderson <sean.martin.anderson@gmail.com>
2014-12-15 18:21:00 -0600

    saved all values to a numpy array

--- pynergy.py
+++ pynergy.py
@@ -15,20 +15,28 @@ BANDS = len(EIGEN[0])
 
 #FILE = open(OUTPUT, 'w')
 energies = []
+kpoints = []
+borigin = []
+btarget = []
 
 #for kpt in range(0, KPTS):
 for kpt in range(0, 1):
     for origin in range(1, BANDS):
         for target in range(origin + 1, BANDS):
             diff = abs(EIGEN[kpt, origin] - EIGEN[kpt, target])
-            entry = '{0:0>3d}.{1:0>2d}->{2:0>2d}'.format(kpt + 1, origin, target)
-            value = '{0:0>9.6f}'.format(diff)
             energies.append(diff)
+            kpoints.append(kpt)
+            borigin.append(origin)
+            btarget.append(target)
+            x = np.array((energies, kpoints, borigin, btarget))
+            #entry = '{0:0>3d}.{1:0>2d}->{2:0>2d}'.format(kpt + 1, origin, target)
+            #value = '{0:0>9.6f}'.format(diff)
             #result = '{1:0>9.6f} | ' \
             #         'k-point: {0:0>3d} | ' \
             #         'bands: {2:0>2d} -> {3:0>2d}\n' \
             #         .format(kpt + 1, diff, origin, target)
             #FILE.write(result)
 
-print energies.index(13.612660000000002)
+#print x
+#print 0.0 <= x <= 0.5
 #FILE.close()
