28e5d5ec128c89192d7a248e713306f6071dad84
Sean Anderson <sean.martin.anderson@gmail.com>
2017-11-22 13:12:33 -0600

    python 3 prints

--- eigenconvert.py
+++ eigenconvert.py
@@ -15,13 +15,13 @@ outfile=sys.argv[2]
 
 def eigenformat(inputfile, outputfile):
     """ formats ABINIT eig file for easy plotting """
-    print "Reading file {0}".format(inputfile)
+    print("Reading file {0}".format(inputfile))
     with open(inputfile, "r") as ifile:
         text = ifile.read().replace('\n', '').replace('kpt#', '\n')
     sub = re.sub(r',.*\)', "", text)
     final = '\n'.join(sub.split('\n')[1:])
     with open(outputfile, "w") as ofile:
         ofile.write(final)
-    print "Created formatted eigenvalue file {0}".format(outputfile)
+    print("Created formatted eigenvalue file {0}".format(outputfile))
 
 eigenformat(infile, outfile)
